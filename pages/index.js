faac45ab3f11f87d995bfd6465bed45377b4221b
anzacsf2010 <40782333+anzacsf2010@users.noreply.github.com>
2021-11-14 21:57:30 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -23,13 +23,9 @@ const Home = ({ allPostsData }) => {
         </Head>
         <section className={utilStyles.headingMd}>
           <p className={utilStyles.introduction}>{ introduction }</p>
-          <p>
-            (This is a sample website - you’ll be building a site like this on{' '}
-            <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-          </p>
         </section>
           <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-              <h2 className={utilStyles.headingLg}>Blog</h2>
+              <h2 className={utilStyles.headingLg}>Blog Section</h2>
               <ul className={utilStyles.list}>
                   {allPostsData.map(({ id, date, title }) => (
                       <li className={utilStyles.listItem} key={id}>
